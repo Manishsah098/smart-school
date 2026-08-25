227294166dbfc4ff75e60d3cc32b07584d67eb93
Manishsah098 <manishsah117618@gmail.com>
2026-08-25 23:04:27 +0530

    added smartschoolapplication.java

---
+++ src/main/java/com/smartschool/SmartSchoolApplication.java
@@ -0,0 +1,12 @@
+package com.smartschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartSchoolApplication.class, args);
+    }
+}
